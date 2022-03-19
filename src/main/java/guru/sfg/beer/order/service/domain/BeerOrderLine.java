09a4245b5f6378b8a7f5ed6d20c38f2f36616a7c
vishchan <vishal.chandrola@amdocs.com>
2022-03-19 19:36:52 +0530

    added upc to beer order line object

--- src/main/java/guru/sfg/beer/order/service/domain/BeerOrderLine.java
+++ src/main/java/guru/sfg/beer/order/service/domain/BeerOrderLine.java
@@ -37,11 +37,12 @@ public class BeerOrderLine extends BaseEntity {
 
     @Builder
     public BeerOrderLine(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate,
-                         BeerOrder beerOrder, UUID beerId, Integer orderQuantity,
+                         BeerOrder beerOrder, UUID beerId,String upc, Integer orderQuantity,
                          Integer quantityAllocated) {
         super(id, version, createdDate, lastModifiedDate);
         this.beerOrder = beerOrder;
         this.beerId = beerId;
+        this.upc = upc;
         this.orderQuantity = orderQuantity;
         this.quantityAllocated = quantityAllocated;
     }
@@ -52,4 +53,5 @@ public class BeerOrderLine extends BaseEntity {
     private UUID beerId;
     private Integer orderQuantity = 0;
     private Integer quantityAllocated = 0;
+    private String upc;
 }
